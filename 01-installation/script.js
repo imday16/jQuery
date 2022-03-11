a77e9444a66ff93c6f961c2e8d1eba52ccc05a68
ayad imad <imday16@gmail.com>
2022-03-11 17:36:52 +0100

    resizable

---
+++ 01-installation/script.js
@@ -0,0 +1,22 @@
+// Equivalent DOMContentLoaded
+/* 
+document.addEventListener('DOMContentLoaded',function(){
+
+});
+*/
+
+$(function(){
+
+    // jQuery est une librairie(bibliothèque) javascript
+    // Slogan : write less, do more
+
+    // jQuery permet de simplifier l'écriture de notre code JS
+
+    // le $ symbolise jQuery
+    // jQuery() équivalent  $()
+
+
+
+
+
+});
