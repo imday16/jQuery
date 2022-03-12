6b9f70134a86e7ef4ddff56bcf737d7f9cd2526e
ayad imad <imday16@gmail.com>
2022-03-12 22:59:52 +0100

    ajout d'index dans installation

--- 01-installation/script.js
+++ 01-installation/script.js
@@ -16,6 +16,46 @@ $(function(){
     // jQuery() équivalent  $()
 
 
+ //Séléction par balise
+ $('span').css('background','blue'); // css()
+ $('span').css({
+     'color' : 'white',
+     'padding':'5px',
+     'margin':'10px 0',
+     'display':'block'
+ });
+
+     //Séléction par id 
+     $('#masection').css('border','1px solid black');
+
+
+     // Selection par classe 
+     $('.avion').css('border','2px solid orange');
+
+     /* je peux sauter des lignes et cumuler l'appel des méthodes */
+     $('.avion')    
+         .eq(1)
+         .css('background','cyan');
+     
+     $('.avion:eq(2)').css('color','red');
+
+     // La méthode on() équivalent à l'addEventListener()
+     // L'écoute de l'évènement
+     $('p').on('click',function(){
+
+         $(this).css('background','lime');
+
+
+     });  
+
+     
+     //let mesParagraphes = document.querySelectorAll('p');
+     //for (let i=0; i < mesParagraphes.length; i++){
+     //    mesParagraphes[i].addEventListener('click','function(){
+     //        this.style.backgroud = 'lime';
+     //  })
+     //}
+     
 
 
 
